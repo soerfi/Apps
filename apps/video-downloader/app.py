3cf624c9e2def6c91c50f4c428cbf24545c973ee
soerfi <soerfi@gmail.com>
2025-12-17 00:44:50 +0100

    feat: standard browser user agent

--- apps/video-downloader/app.py
+++ apps/video-downloader/app.py
@@ -85,6 +85,11 @@ def fetch_video():
             'outtmpl': os.path.join(current_download_dir, 'original.%(ext)s'),
             'noplaylist': True,
             'quiet': True,
+            'http_headers': {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                'Accept-Language': 'en-us,en;q=0.5',
+            }
         }
         
         cookie_file = os.path.join(CONFIG_FOLDER, 'cookies.txt')
